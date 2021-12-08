d21b6bb858808803eef9a45e76da236a1b7c8e3b
Alfred Garcia <garcia.alfredm@gmail.com>
2021-12-07 20:01:55 -0500

    users model

---
+++ src/main/java/models/Users.java
@@ -0,0 +1,94 @@
+package models;
+
+public class Users {
+    Integer id;
+    String username;
+    String password;
+    String firstName;
+    String LastName;
+    String email;
+    Integer roleId;
+    //model composite key of username, email
+
+    public Users() {
+    }
+
+    public Users(Integer id, String username, String password, String firstName, String lastName, String email, Integer roleId) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        LastName = lastName;
+        this.email = email;
+        this.roleId = roleId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", email='" + email + '\'' +
+                ", roleId=" + roleId +
+                '}';
+    }
+}
